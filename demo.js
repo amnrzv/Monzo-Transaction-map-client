117bcb704eaacadab71e3db16c5a08bc5917f2e6
DESKTOP-5FPBSN9\amaan <amaan1712@gmail.com>
2018-06-21 17:44:56 +0530

    - Testing plotly and mapbox with dummy data - Added plotly source

---
+++ demo.js
@@ -0,0 +1,30 @@
+var data = [{
+    type:'scattermapbox',
+    lat:['45.5017', '47'],
+    lon:['-73.5673', '-75'],
+    mode:'markers',
+    marker: {
+      size:14
+    },
+    text:['Montreal', 'Rizvi']
+  }]
+  
+  var layout = {
+    autosize: true,
+    hovermode:'closest',
+    mapbox: {
+      bearing:0,
+      center: {
+        lat:45,
+        lon:-73
+      },
+      pitch:0,
+      zoom:2
+    },
+  }
+  
+  Plotly.setPlotConfig({
+    mapboxAccessToken: 'pk.eyJ1IjoiYW1ucnp2IiwiYSI6ImNqaW82ZG1paDBrcmszcHB1MWVnam8xOWwifQ.hOT9ly8lAV31UjB7FxwmqA'
+  })
+  
+  Plotly.plot('tester', data, layout)
--- main.js
+++ main.js
@@ -4,9 +4,9 @@ let sanitized_data = sample.transactions
     .filter(x => x.settled !== "" && x.amount < 0 && x.merchant)
     .map(x => {
         const { amount, category, merchant: { address: { latitude, longitude, country } } } = x;
-        return {
+        return [
             amount, category, country, latitude, longitude
-        }
+        ]
     })
 
 console.log(sanitized_data)
