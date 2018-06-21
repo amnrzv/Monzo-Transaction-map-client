7f1f76fca79c53c0ff6162d5bff398edff4f9756
DESKTOP-5FPBSN9\amaan <amaan1712@gmail.com>
2018-06-21 14:25:07 +0530

    initial commit

---
+++ filter-data.js
@@ -0,0 +1,12 @@
+const sample = require('./sample-data.json')
+
+let sanitized_data = sample.transactions
+    .filter(x => x.settled !== "" && x.amount < 0 && x.merchant)
+    .map(x => {
+        const { amount, category, merchant: { address: { latitude, longitude, country } } } = x;
+        return {
+            amount, category, country, latitude, longitude
+        }
+    })
+
+console.log(sanitized_data)
